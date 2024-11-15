664d928d7dfc3ac4604417c61b6b368b42df425b
Kaue Samartino <134535906+Kauesamartino@users.noreply.github.com>
2024-11-15 05:56:08 -0300

    Update code

--- GlobalSolution/js/script.js
+++ GlobalSolution/js/script.js
@@ -1,8 +1,4 @@
 function toggleMenu() {
     var barraOpcoes = document.getElementById("barraOpcoes");
-    if (barraOpcoes.style.display === "none" || barraOpcoes.style.display === "") {
-        barraOpcoes.style.display = "block"; // Mostra a barra de opções
-    } else {
-        barraOpcoes.style.display = "none"; // Esconde a barra de opções
-    }
+    barraOpcoes.classList.toggle("active");
 }
