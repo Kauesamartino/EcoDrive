d8803f654b4a2568e724f4430ff86c8a14e1e105
Kaue Samartino <134535906+Kauesamartino@users.noreply.github.com>
2025-03-31 14:56:26 -0300

    Update WebSite

--- js/script.js
+++ js/script.js
@@ -5,32 +5,32 @@ function toggleMenu() {
 
 function validarFormContato(event) {
     event.preventDefault();
-    
+
     const nome = document.getElementById('txtNome').value.trim();
     const sobrenome = document.getElementById('txtSobrenome').value.trim();
     const email = document.getElementById('txtEmail').value.trim();
     const mensagem = document.getElementById('mensagem').value.trim();
-    
+
     if (!nome) {
         alert('Por favor, preencha o nome.');
         return false;
     }
-    
+
     if (!sobrenome) {
         alert('Por favor, preencha o sobrenome.');
         return false;
     }
-    
+
     if (!email) {
         alert('Por favor, preencha o e-mail.');
         return false;
     }
-    
+
     if (!mensagem) {
         alert('Por favor, escreva uma mensagem.');
         return false;
     }
-    
+
     setTimeout(() => {
         window.location.href = 'mensagem-enviada.html';
     }, 100);
@@ -40,26 +40,26 @@ function validarFormContato(event) {
 
 function validarFormOrcamento(event) {
     event.preventDefault();
-    
+
     const nome = document.getElementById('txtNome').value.trim();
     const email = document.getElementById('txtEmail').value.trim();
     const mensagem = document.getElementById('mensagem').value.trim();
-    
+
     if (!nome) {
         alert('Por favor, preencha o nome.');
         return false;
     }
-    
+
     if (!email) {
         alert('Por favor, preencha o e-mail.');
         return false;
     }
-    
+
     if (!mensagem) {
         alert('Por favor, informe os carros e quantidades desejados.');
         return false;
     }
-    
+
     setTimeout(() => {
         window.location.href = 'orcamento-enviado.html';
     }, 100);
